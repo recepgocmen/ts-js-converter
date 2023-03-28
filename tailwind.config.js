791c70b38713bf7e687df40f7b9502c250813d07
Recep GOCMEN <recep.gocmen@b2metric.com>
2023-03-28 23:20:34 +0300

    config eslint,tailwind,package.json & boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export const mode = "jit";
+export const purge = ["./src/**/*.{js,ts,jsx,tsx}"];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const variants = {};
+export const plugins = [];
